3d9641eaa00b1b6e6d66bfdee90a95eecd9f5d37
qiucoolcool <qiutengcool@163.com>
2021-11-30 14:34:38 +0800

    Update load_data.m

--- Externel/load_data.m
+++ Externel/load_data.m
@@ -6,7 +6,7 @@ if any(strcmp(dataName,{'PenDigits','MNIST'}))
     clear fea; clear gt;
 elseif any(strcmp(dataName,{'cytof_h1','cytof_h2','cytof_one','Samusik_01','Samusik_all'}))
     load([dataName,'.mat'],'data','annotation_data') 
-elseif any(strcmp(dataName,{'gauss_spiral_circle_dataWithLabel','gauss_spiral_circle_data_in_noiseWithLabel','AGG','Flame','Spiral','Jain','2G','2G_unbalance','S1','R15','3Circles','S1_001S1','UB'}))
+elseif any(strcmp(dataName,{'gauss_spiral_circle_dataWithLabel','gauss_spiral_circle_data_in_noiseWithLabel','AGG','Flame','Spiral','Jain','2G','2G_unbalance','S1','R15','3Circles','S1_001S1','UB','One_Dim_uniform_data_1048576'}))
     load(['Random_permuted_', dataName,'.mat'],'data','annotation_data')
 elseif any(strcmp(dataName,{'data_TB1M','data_SF2M','data_TB_16384','data_TB_32768','data_TB_65536','data_TB_131072','data_TB_262144','data_TB_524288','data_TB_1048576','data_SF_16384','data_SF_32768','data_SF_65536','data_SF_131072','data_SF_262144','data_SF_524288','data_SF_1048576','data_CC_16384','data_CC_32768','data_CC_65536','data_CC_131072','data_CC_262144','data_CC_524288','data_CC_1048576','data_CG_16384','data_CG_32768','data_CG_65536','data_CG_131072','data_CG_262144','data_CG_524288','data_CG_1048576','data_Flower_16384','data_Flower_32768','data_Flower_65536','data_Flower_131072','data_Flower_262144','data_Flower_524288','data_Flower_1048576','data_TB_1000000', 'data_SF_1000000','data_CC_1000000', 'data_CG_1000000', 'data_Flower_1000000','data_TB_100000', 'data_SF_100000','data_CC_100000', 'data_CG_100000', 'data_Flower_100000'}))
     load([dataName,'.mat']) % 
